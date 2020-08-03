30bc800972840d59b82095f31b49cddb2d0f6530
Stephen Ajulu <alunje73@gmail.com>
2020-08-03 12:33:18 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteUrl: process.env.SITE_URL || `https://status.lekoarts.de`,
-    siteName: process.env.SITE_TITLE || `Status Dashboard - LekoArts`,
-    siteDescription: process.env.SITE_DESCRIPTION || `Showing the statuses of my Netlify deploys & CircleCI tests.`,
+    siteUrl: process.env.SITE_URL || `https://status.stephenajulu.com`,
+    siteName: process.env.SITE_TITLE || `Project Status Dashboard - Stephen Ajulu`,
+    siteDescription: process.env.SITE_DESCRIPTION || `Showing the status view of all my netlify projects. Here you can see whether a project is building, getting queued, no builds or is failing.`,
   },
   plugins: [
     // See the theme's README for all available components
